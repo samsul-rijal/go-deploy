d8caab3f054634547b961474b431f7309be47f19
Your Name <samsulr99@gmail.com>
2022-08-30 10:57:00 +0700

    update

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	// env
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		panic("Failed to load env file")
+	}
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -14,5 +24,9 @@ func main() {
 		})
 	})
 
-	r.Run("localhost:8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	var port = os.Getenv("PORT")
+
+	fmt.Println("server running localhost:" + port)
+
+	r.Run(":" + port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
